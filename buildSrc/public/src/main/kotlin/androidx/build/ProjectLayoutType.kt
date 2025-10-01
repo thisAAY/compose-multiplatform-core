8ab2d2f22cdc808acf82ac103a64ab118862ba96
Ivan Matkov <ivan.matkov@jetbrains.com>
2025-10-01 14:52:32 +0200

    Do not require offline structure from repo-tool checkout

--- buildSrc/public/src/main/kotlin/androidx/build/ProjectLayoutType.kt
+++ buildSrc/public/src/main/kotlin/androidx/build/ProjectLayoutType.kt
@@ -21,7 +21,8 @@ import org.gradle.api.Project
 
 enum class ProjectLayoutType {
     ANDROIDX,
-    PLAYGROUND;
+    PLAYGROUND,
+    JETBRAINS_FORK;
 
     companion object {
         /** Returns the project layout type for the project (PLAYGROUND or ANDROIDX) */
@@ -30,8 +31,9 @@ enum class ProjectLayoutType {
             val value = project.extraPropertyOrNull(STUDIO_TYPE)
             return when (value) {
                 "playground" -> PLAYGROUND
-                null,
                 "androidx" -> ANDROIDX
+                null,
+                "jetbrains-fork" -> JETBRAINS_FORK
                 else -> error("Invalid project type $value")
             }
         }
@@ -39,7 +41,14 @@ enum class ProjectLayoutType {
         /** @return `true` if running in a Playground (Github) setup, `false` otherwise. */
         @JvmStatic
         fun isPlayground(project: Project): Boolean {
-            return ProjectLayoutType.from(project) == PLAYGROUND
+            return when(ProjectLayoutType.from(project)) {
+                ANDROIDX -> false
+                PLAYGROUND -> true
+
+                // This check is used to determine if prebuilt dependencies are available,
+                // so despite of applying playground gradle plugin, we need to return true here
+                JETBRAINS_FORK -> true
+            }
         }
     }
 }

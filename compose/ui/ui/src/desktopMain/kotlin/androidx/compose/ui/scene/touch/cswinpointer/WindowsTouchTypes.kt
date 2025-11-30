4d5473c4d97f73d2c4eaacd5593305e26e20121e
Ahmed Ali <ahmedaliaay@gmail.com>
2025-11-30 19:50:59 +0200

    Remove unneeded functions

--- compose/ui/ui/src/desktopMain/kotlin/androidx/compose/ui/scene/touch/cswinpointer/WindowsTouchTypes.kt
+++ compose/ui/ui/src/desktopMain/kotlin/androidx/compose/ui/scene/touch/cswinpointer/WindowsTouchTypes.kt
@@ -41,14 +41,6 @@ internal object WindowsTouchConstants {
  */
 internal interface TouchUser32 : User32 {
     /**
-     * Determines whether the specified window handle identifies an existing window.
-     *
-     * @param hWnd A handle to the window to test.
-     * @return True if the window handle identifies an existing window, false otherwise.
-     */
-    fun IsWindow(hWnd: WinDef.HWND): Boolean
-
-    /**
      * Retrieves the identifier of the thread that created the specified window.
      *
      * @param hWnd A handle to the window.
